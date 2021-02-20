c5abb6b5ae3cda58ca0228d7c8ed30d674960a62
bethcreate <kamaubetth@gmail.com>
2021-02-21 00:00:09 +0300

    my first assignment

---
+++ src/mobiledevelopment.kt
@@ -0,0 +1,12 @@
+fun main () {
+    var fullname= "jackline"
+    println(fullname)
+    var age= 20
+    println(age)
+    var phonenumber= "0769209586"
+    println(phonenumber)
+    var weightinkgs= 50
+    println(weightinkgs)
+    var citizen: Boolean= true
+    println(citizen)
+}
